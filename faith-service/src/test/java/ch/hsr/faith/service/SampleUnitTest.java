1840daac202952e23d0fe8f95cc43d75cfe10d25
Stefan Kapferer <stefan@kapferer.ch>
2014-03-02 19:12:48 +0100

    Initial Maven Project Setup

---
+++ faith-service/src/test/java/ch/hsr/faith/service/SampleUnitTest.java
@@ -0,0 +1,15 @@
+package ch.hsr.faith.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleUnitTest {
+
+	@Test
+	public void test() {
+		// Just a small sample test
+		assertTrue(true);
+	}
+
+}
