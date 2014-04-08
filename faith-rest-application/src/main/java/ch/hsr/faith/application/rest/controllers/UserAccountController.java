5f93b5236aa0e43c38a5bd192c4d601c840d0858
Stefan Kapferer <stefan@kapferer.ch>
2014-04-08 10:24:53 +0200

    Cleaned POM-Files: Spring-Security-Version configured on one single place

--- faith-rest-application/src/main/java/ch/hsr/faith/application/rest/controllers/UserAccountController.java
+++ faith-rest-application/src/main/java/ch/hsr/faith/application/rest/controllers/UserAccountController.java
@@ -3,8 +3,6 @@ package ch.hsr.faith.application.rest.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
