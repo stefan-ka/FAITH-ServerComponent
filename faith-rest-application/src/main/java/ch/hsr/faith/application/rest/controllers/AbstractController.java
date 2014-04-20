cf54751db9e2e67860e24333103b2865fc1bd9fa
Dominic <dominicmh@gmail.com>
2014-04-20 18:25:48 +0200

    Updates on insert SQL statements

--- faith-rest-application/src/main/java/ch/hsr/faith/application/rest/controllers/AbstractController.java
+++ faith-rest-application/src/main/java/ch/hsr/faith/application/rest/controllers/AbstractController.java
@@ -1,14 +1,8 @@
 package ch.hsr.faith.application.rest.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.View;
-
 import ch.hsr.faith.application.rest.dto.BaseJSONResponse;
 
 public abstract class AbstractController {
-
-	@Autowired
-	protected View jsonView;
 
 	protected BaseJSONResponse createResponse(String status, Object data) {
 		BaseJSONResponse response = new BaseJSONResponse(status);
