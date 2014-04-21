7f2c5e6f7ee338308ffd42dd7660c3567a2259a3
Stefan Kapferer <stefan@kapferer.ch>
2014-04-21 09:14:44 +0200

    Implemented Service to load all facility categories

---
+++ faith-service/src/main/java/ch/hsr/faith/service/FacilityCategoryService.java
@@ -0,0 +1,11 @@
+package ch.hsr.faith.service;
+
+import java.util.List;
+
+import ch.hsr.faith.domain.FacilityCategory;
+
+public interface FacilityCategoryService {
+
+	public List<FacilityCategory> findAll();
+
+}
---
+++ faith-service/src/main/java/ch/hsr/faith/service/impl/FacilityCategoryServiceImpl.java
@@ -0,0 +1,23 @@
+package ch.hsr.faith.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ch.hsr.faith.domain.FacilityCategory;
+import ch.hsr.faith.repository.FacilityCategoryRepository;
+import ch.hsr.faith.service.FacilityCategoryService;
+
+@Service
+public class FacilityCategoryServiceImpl implements FacilityCategoryService {
+
+	@Autowired
+	private FacilityCategoryRepository facilityCategoryRepository;
+
+	@Override
+	public List<FacilityCategory> findAll() {
+		return facilityCategoryRepository.findAll();
+	}
+
+}
