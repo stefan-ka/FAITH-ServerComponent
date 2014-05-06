e0a10ef01f7b143942ed8aaa139c43bc23764115
Stefan Kapferer <stefan@kapferer.ch>
2014-05-06 21:29:58 +0200

    JpaFacilityRepository: Fixed distinct problem in Query

--- faith-repository/src/main/java/ch/hsr/faith/repository/impl/jpa/JpaFacilityRepository.java
+++ faith-repository/src/main/java/ch/hsr/faith/repository/impl/jpa/JpaFacilityRepository.java
@@ -21,7 +21,7 @@ public interface JpaFacilityRepository extends JpaRepository<Facility, Long>, Jp
 
 	public abstract List<Facility> findByFacilityCategory(FacilityCategory facilityCategory);
 
-	@Query(value = "SELECT f from Facility f, PieceOfFurniture p, ItemNeeded i where f.id = i.facility.id and p.id = i.pieceOfFurniture.id and p.id = :pieceOfFurnitureId")
+	@Query(value = "SELECT DISTINCT f from Facility f, PieceOfFurniture p, ItemNeeded i where f.id = i.facility.id and p.id = i.pieceOfFurniture.id and p.id = :pieceOfFurnitureId")
 	public abstract List<Facility> findByPieceOfFurnitureNeededId(@Param(value = "pieceOfFurnitureId") Long pieceOfFurnitureId);
 
 }
