e9b09bf5be6afc8c2e1509bbbfb862994a104bea
Stefan Kapferer <stefan@kapferer.ch>
2014-05-15 10:11:27 +0200

    Added Service for Blitz Authorization

--- faith-rest-application/src/main/java/ch/hsr/faith/application/rest/controllers/HelloController.java
+++ faith-rest-application/src/main/java/ch/hsr/faith/application/rest/controllers/HelloController.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,9 +18,9 @@ public class HelloController extends AbstractController {
 
 	@Autowired
 	private UserAccountService userAccountService;
-	
+
 	Logger logger = Logger.getRootLogger();
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
 	public BaseJSONResponse showProduct(Model model) {
@@ -29,6 +30,12 @@ public class HelloController extends AbstractController {
 		return message;
 	}
 
+	@RequestMapping(value = "/mu-a8bdc266-28765094-58641ee2-a510e9ca", method = RequestMethod.GET)
+	@ResponseBody
+	public String authorizeBlitz() {
+		return "42";
+	}
+
 	@RequestMapping(value = "/secured", method = RequestMethod.GET)
 	@ResponseBody
 	public BaseJSONResponse showSecuredMethod(Model model) {
@@ -36,7 +43,7 @@ public class HelloController extends AbstractController {
 		message.setData(userAccountService.secureMethod());
 		return message;
 	}
-	
+
 	@RequestMapping(value = "/nonsecured", method = RequestMethod.GET)
 	@ResponseBody
 	public BaseJSONResponse showNonSecuredMethod(Model model) {
